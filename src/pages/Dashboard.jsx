5bb3bb11399a9ae598b4640c0647327a585ac6d1
Diego Oliveira de Carvalho <Diego.Oliveira@cfq.org.br>
2024-10-04 09:30:51 -0300

    Paginas de login e dashboard criadas

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,2 +1,16 @@
-function Dashboard() {}
+import AppNav from "../components/AppNav";
+import styles from "./Dashboard.module.css";
+function Dashboard() {
+  return (
+    <>
+      <AppNav />
+      <section className={styles.dashContent}>
+        <div>
+          <p>Numero de projetos</p>
+          <h1>82</h1>
+        </div>
+      </section>
+    </>
+  );
+}
 export default Dashboard;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,2 +1,21 @@
-function Login() {}
+import styles from "./Login.module.css";
+function Login() {
+  return (
+    <main className={styles.loginPage}>
+      <form className={styles.loginForm}>
+        <div>
+          <label>Login</label>
+          <input type="text"></input>
+        </div>
+        <div>
+          <label>Senha</label>
+          <input type="text"></input>
+        </div>
+        <div>
+          <button className={styles.loginBtn}>Entrar</button>
+        </div>
+      </form>
+    </main>
+  );
+}
 export default Login;
